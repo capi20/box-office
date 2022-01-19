6128547e09ba97e50bf1b59904705a2b6442b575
shyam <shyam20998@gmail.com>
2022-01-19 17:23:01 +0530

    show results and refactor api code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
+import {apiGet} from '../misc/config'
 
 function Home() {
     const [input, setInput] = useState('')
+    const [results, setResults] = useState(null)
 
     const onSearch = () => {
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-            .then(result => result.json())
-            .then(data => console.log(data))
+        apiGet(`/search/shows?q=${input}`).then(result => setResults(result))
     }
-    
+
     const onInputChange = (e) => {
         setInput(e.target.value)
     }
@@ -20,10 +20,27 @@ function Home() {
         }
     }
 
+    const renderResults = () => {
+        if (results && results.length === 0) {
+            return <div>No results</div>
+        }
+
+        if (results && results.length > 0) {
+            return (
+                <div>
+                    {results.map(el => <div key={el.show.id}>{el.show.name}</div>)}
+                </div>
+            )
+        }
+
+        return null;
+    }
+
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
         </MainPageLayout>
     )
 }
