29c44f46b405a0f40115fd516b4bebea6970ff23
shyam <shyam20998@gmail.com>
2022-01-19 17:08:45 +0530

    added input and integrated tvmaze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 function Home() {
+    const [input, setInput] = useState('')
+
+    const onSearch = () => {
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+            .then(result => result.json())
+            .then(data => console.log(data))
+    }
+    
+    const onInputChange = (e) => {
+        setInput(e.target.value)
+    }
+
+    const onKeyDown = e => {
+        if (e.keyCode === 13) {
+            onSearch()
+        }
+    }
+
     return (
         <MainPageLayout>
-            This is home
+            <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+            <button type="button" onClick={onSearch}>Search</button>
         </MainPageLayout>
     )
 }
