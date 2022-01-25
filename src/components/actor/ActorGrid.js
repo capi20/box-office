bacfe1e13d50bf5faf5424858abf8f6f5e608c5b
shyam <shyam20998@gmail.com>
2022-01-25 15:59:50 +0530

    installed styled components and created styles for search card

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { FlexGrid } from '../styled'
 import ActorCard from './ActorCard'
 
 function ActorGrid({data}) {
     return (
-        <div>
+        <FlexGrid>
             {data.map(({person}) => (
                 <ActorCard
                     key={person.id}
@@ -16,7 +17,7 @@ function ActorGrid({data}) {
                     gender={person.gender}
                     image={person.image ? person.image.medium : IMAGE_NOT_FOUND}/>
             ))}
-        </div>
+        </FlexGrid>
     )
 }
 
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ShowCard from './ShowCard'
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { FlexGrid } from '../styled'
 
 function ShowGrid({ data }) {
     return (
-        <div>
+        <FlexGrid>
             {data.map(({show}) => (
                 <ShowCard
                     key={show.id}
@@ -14,7 +15,7 @@ function ShowGrid({ data }) {
                     image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
                     summary={show.summary}/>
             ))}
-        </div>
+        </FlexGrid>
     )
 }
 
